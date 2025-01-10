52ff77e81d90bb88ecca4bca38787cfd74668321
francescorago <151189047+francescorago@users.noreply.github.com>
2025-01-10 10:33:15 +0100

    .

--- ackgestionedocumentale/src/components/DocumentCreation.js
+++ ackgestionedocumentale/src/components/DocumentCreation.js
@@ -1285,7 +1285,7 @@ const renderSpecificFields = () => {
                 </button>
                 <button
                   className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-                  onClick={handleSave}
+                  onClick={handleSave & handleHome}
                 >
                   Salva
                 </button>
